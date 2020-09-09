5f3b7b0286c9deb8f3fa112ac16e6aeb8ab21781
Daniel Etaat <34327451+DanielEtat@users.noreply.github.com>
2020-09-09 01:46:16 -0700

    added json data

--- js/topics.js
+++ js/topics.js
@@ -1,11 +1,21 @@
 var subject;
 var selectedTopics = [];
+var topics;
+
+// Creating Subjects Array
+var subjects = {};
+subjects["Algebra 1"] = ["Add Integers (small numbers)", "Add Integers (large numbers)", "Subtract Integers (small numbers)", "Subtract Integers (large numbers)", "Multiply Integers (small numbers)", "Multiply Integers (large numbers)", "Divide Integers (small numbers)", "Divide Integers (large numbers)", "Order of Operations with Integers", "Order of Operations", "Order of Operations with Rational Numbers", "Add Rational Numbers", "Subtract Rational Numbers", "Multiply Rational Numbers", "Divide Rational Numbers", "Square Roots", "Cube Roots", "Equivalent Ratios", "Proportions", "Finding the midpoint between two points", "Finding the endpoint", "Finding the distance between two points"];
+subjects["Algebra 2"] = ["Adding and Subtracting Complex Numbers", "Adding and Subtracting Complex Numbers with Rational Numbers", "Finding the Complex Conjugate", "Multiplying Complex Numbers", "Dividing Complex Numbers", "Absolute Value of Complex Numbers", "Powers of i", "Solve quadratics using the quadratic formula", "Find the discriminant", "Solve quadratics by factoring", "Write a quadratic equation from roots"];
+subjects["Pre-Calculus"] = ["Radian to Degree Conversion", "Degree to Radian Conversion", "Finding Reference Angles in Radians", "Finding Reference Angles in Degrees", "Finding Coterminal Angles in Radians", "Finding Coterminal Angles in Degrees", "Finding ratios of trigonometric functions (in radians)", "Finding ratios of trigonometric functions (in degrees)", "Finding inverses of trigonometric functions (in radians)", "Finding inverses of trigonometric functions (in degrees)"];
+
 
 window.onload = function() {
-  console.log("getting arguments.");
+  console.log('getting arguments.');
   var params = getParams(window.location.href);
-  subject = params["subject"];
+  subject = params['subject'];
+  topics = subjects[subject];
   addSubject();
+  addTopics();
 }
 
 var getParams = function(url) {
@@ -22,35 +32,62 @@ var getParams = function(url) {
 };
 
 var addSubject = function() {
-  var element = document.getElementById("subject");
+  var element = document.getElementById('subject');
   element.innerText = subject;
-}
+};
+
+var addTopics = function() {
+  var e = document.getElementById("topics");
+  for (var i = 0; i < topics.length; i++) {
+    var topicNode = document.createElement("div");
+    var buttonNode = document.createElement("button");
+    var t = topics[i];
+    topicNode.classList.add("col-12");
+    topicNode.classList.add("col-lg-4");
+    topicNode.classList.add("col-md-6");
+    buttonNode.classList.add("btn-topic");
+    buttonNode.id = topics[i];
+    buttonNode.onclick = createSelectTopic(topics[i]);
+    buttonNode.innerText = topics[i]
+    topicNode.appendChild(buttonNode);
+    e.appendChild(topicNode);
+  }
+};
+
+var createSelectTopic = function(id) {
+  var callSelectTopic = function() {
+    var element = document.getElementById(id);
+    selectTopic(element);
+  }
+  return callSelectTopic;
+};
+
 
 var selectTopic = function(element) {
   var topic = element.innerText;
-  if (element.classList.contains("selected")) {
-    element.classList.remove("selected");
+  if (element.classList.contains('selected')) {
+    element.classList.remove('selected');
     selectedTopics.splice(selectedTopics.indexOf(topic), 1);
   } else {
-    element.classList.add("selected");
+    element.classList.add('selected');
     selectedTopics.push(topic);
   }
-  var e = document.getElementById("topics-list");
+  var e = document.getElementById('topics-list');
   e.innerText = selectedTopicsToString(selectedTopics);
-}
+};
 
 var selectedTopicsToString = function(topics) {
-  var string = "";
+  var string = '';
   for (var i = 0; i < topics.length; i++) {
     if (i == topics.length-1 && i != 0) {
-      string += "and " + topics[i] + ".";
+      string += 'and ' + topics[i] + '.';
     } else if (topics.length == 1) {
-      string += topics[i] + ".";
+      string += topics[i] + '.';
     } else if (topics.length == 2 && i == 0) {
-      string += topics[i] + " ";
+      string += topics[i] + ' ';
     } else {
-      string += topics[i] + ", "
+      string += topics[i] + ', '
     }
   }
   return string;
-}
+};
