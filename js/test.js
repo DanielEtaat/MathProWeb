4f47f720ce2ce491d710b92c524c62e80c301e20
Daniel Etaat <34327451+DanielEtat@users.noreply.github.com>
2020-10-10 16:29:29 -0700

    Update test.js

--- js/test.js
+++ js/test.js
@@ -3,9 +3,9 @@ var convertInput = function() {
   var e = document.getElementById("text");
   questionList.push(e.value);
   var url = "/MathProgramWebsite/pdf.html?";
-  url += "questionList=" + questionList.join(";");
-  url += "&answerList=" + answerList.join(";");
-  url += "&instructionList=" + instructionList.join(";");
-  url += "&topicList=" + topicList.join(";");
+  url += "questionList=" + [].join(";");
+  url += "&answerList=" + [].join(";");
+  url += "&instructionList=" + [].join(";");
+  url += "&topicList=" + [].join(";");
   window.location.href = url;
 }
