bbd09a5e18f721545a3e328de7c8ad54ff7862c2
GianniGabriel <giannigab789@gmail.com>
2021-09-01 14:52:42 -0400

    Iteratively Add Wkst to PDF

--- client/src/components/pages/WorksheetCustomization/DisplayPDF.js
+++ client/src/components/pages/WorksheetCustomization/DisplayPDF.js
@@ -10,41 +10,47 @@ import Spinner from "../../layout/Spinner";
 
 import { pdf } from "../PDF/PDF.module.css";
 import { buttonLeft, buttonRight } from "./WorksheetCustomization.module.css";
-import { back, next } from "./Button.module.css";
+import { back, prenext, next } from "./Button.module.css";
 
 const DisplayPDF = () => {
   const { subjectName } = useParams();
   const { topicCart } = useContext(CartContext);
   const { loading, jsonString, getQuestions } = useContext(WorksheetContext);
   const [topics, setTopics] = useState([]);
+  const [isDownloading, setIsDownloading] = useState(false);
 
   const pdfContainer = useRef(null);
 
   const onDownload = () => {
-    // TODO: ADD DOWNLOAD LOADING ANIMATION
-    jsonString && console.log("pdf downloading...");
-
+    setIsDownloading(true);
+    document.body.style.overflow = "hidden";
     pdfContainer.current.style.overflow = "visible";
-    pdfContainer.current.style.height = `${
-      pdfContainer.current.scrollHeight * 1.5
-    }px`;
+    pdfContainer.current.style.height = `${pdfContainer.current.scrollHeight}px`;
     html2canvas(pdfContainer.current).then((canvas) => {
       const imgData = canvas.toDataURL("image/png");
+      const imgWidth = 210;
+      const pageHeight = 295;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      let heightLeft = imgHeight;
+      let position = 0;
 
       // Generate PDF
-      const doc = new jsPDF("p", "px", "a1");
-      doc.addImage(
-        imgData,
-        "PNG",
-        0,
-        0,
-        1080,
-        pdfContainer.current.scrollHeight
-      );
+      const doc = new jsPDF("p", "mm");
+      heightLeft -= pageHeight;
+      doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+      while (heightLeft >= 0) {
+        position = heightLeft - imgHeight; // top padding for other pages
+        doc.addPage();
+        doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
       doc.save("worksheet.pdf");
+      setIsDownloading(false);
+
+      document.body.style.overflow = "hidden";
+      pdfContainer.current.style.overflow = "scroll";
+      pdfContainer.current.style.height = "auto";
     });
-    pdfContainer.current.style.overflow = "scroll";
-    pdfContainer.current.style.height = "auto";
   };
 
   useEffect(() => {
@@ -72,8 +78,12 @@ const DisplayPDF = () => {
         </Link>
       </div>
       <div className={buttonRight}>
-        <button className={next} onClick={onDownload}>
-          Download PDF
+        <button
+          disabled={isDownloading || loading}
+          className={isDownloading || loading ? prenext : next}
+          onClick={onDownload}
+        >
+          {isDownloading ? "Downloading..." : "Download PDF"}
         </button>
       </div>
     </div>
