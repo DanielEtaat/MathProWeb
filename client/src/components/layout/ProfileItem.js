d3be234744dddcc46d710efa663b3f830df614d6
GianniGabriel <giannigab789@gmail.com>
2021-09-05 19:25:59 -0400

    Change design and add flip function

---
+++ client/src/components/layout/ProfileItem.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import {
+  profileItemCard,
+  profileItemContainer,
+  profileItem,
+  profileItemBack,
+  profileItemFlip,
+  profileTextContainer,
+  profileImage,
+  profileName,
+  profileTitle,
+  profileLink,
+} from "./AboutUsModal.module.css";
+
+const ProfileItem = ({ name, title, imageSrc, linkedinURL, githubURL }) => {
+  const [isBackShown, setIsBackShown] = useState(false);
+
+  const onProfileClick = (e) => {
+    switch (e.target.id) {
+      case "profile-container":
+      case "profile-card":
+      case "profile-front":
+      case "profile-back":
+      case "profile-image":
+      case "profile-name":
+      case "profile-title":
+        e.preventDefault();
+        setIsBackShown(!isBackShown);
+        break;
+    }
+  };
+
+  return (
+    <div
+      className={profileItemContainer}
+      id="profile-container"
+      onClick={onProfileClick}
+    >
+      <div
+        className={`${profileItemCard} ${isBackShown ? profileItemFlip : ""}`}
+        id="profile-card"
+      >
+        <div className={profileItem} id="profile-front">
+          {imageSrc && (
+            <img
+              src={imageSrc}
+              alt="profile"
+              className={profileImage}
+              id="profile-image"
+            />
+          )}
+          <div className={profileTextContainer}>
+            <h2 className={profileName} id="profile-name">
+              {name}
+            </h2>
+            <h3 className={profileTitle} id="profile-title">
+              {title}
+            </h3>
+          </div>
+        </div>
+        <div className={profileItemBack} id="profile-back">
+          {linkedinURL && (
+            <a
+              className={profileLink}
+              id="linkedin-link"
+              target="_blank"
+              href={linkedinURL}
+            >
+              <i className="fab fa-linkedin" id="linkedin-icon"></i>
+            </a>
+          )}
+          {githubURL && (
+            <a
+              className={profileLink}
+              id="github-link"
+              target="_blank"
+              href={githubURL}
+            >
+              <i className="fab fa-github-square" id="github-icon"></i>
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProfileItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
+  linkedinURL: PropTypes.string,
+  githubURL: PropTypes.string,
+};
+
+export default ProfileItem;
